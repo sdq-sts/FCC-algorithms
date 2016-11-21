6f01818eb8367991dae51db7d0ec1123913eaf3e
Sadraque <sdq.sts@gmail.com>
2016-11-21 12:15:18 -0200

    I'm A Little Tea Pot

---
+++ Basic/tittle-case-a-sentence.js
@@ -0,0 +1,20 @@
+/*
+Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+
+For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
+*/
+
+function titleCase(str) {
+  let arr = str.toLowerCase().split(" ")
+
+  let na = arr.map((el) => {
+    let elArr = el.split('').slice(1).join('')
+    let elFirstChar = el.charAt(0).toUpperCase()
+
+    return elFirstChar + elArr
+  })
+
+  return console.log(na.join(' '))
+}
+
+titleCase("I'm a little tea pot") // => I'm A Little Tea Pot
