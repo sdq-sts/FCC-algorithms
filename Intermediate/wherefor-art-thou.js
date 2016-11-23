bb331e8b8c8cb082afdef929f6e4ccc4308f9e6b
Sadraque <sdq.sts@gmail.com>
2016-11-23 04:45:39 -0200

    Wherefor Art Thou

---
+++ Intermediate/wherefor-art-thou.js
@@ -0,0 +1,24 @@
+/*
+Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching property and value pairs (second argument). Each property and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
+For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the property and its value, that was passed on as the second argument.
+*/
+
+function whatIsInAName(collection, source) {
+  let arr = []
+  let sourceKeys = Object.keys(source)
+
+  arr = collection.filter((obj) => {
+    let objItem = []
+
+    objItem = sourceKeys.every((key) => {
+      return obj.hasOwnProperty(key) && obj[key] === source[key]
+    })
+
+    return objItem
+  })
+
+  return arr
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" })
