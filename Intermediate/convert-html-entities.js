3d7fccc31b3c5d55a056d02feab3e45aadb83ca3
Sadraque <sdq.sts@gmail.com>
2016-11-25 03:43:27 -0200

    Convert html entities

---
+++ Intermediate/convert-html-entities.js
@@ -0,0 +1,15 @@
+/*
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+*/
+
+function convertHTML (str) {
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;')
+}
+
+convertHTML('Dolce & Gabbana') // => Dolce &amp; Gabbana
+convertHTML('Hamburgers < Pizza < Tacos') // => Hamburgers &lt; Pizza &lt; Tacos
