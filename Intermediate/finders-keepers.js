28613504ba29acb58e49f8dc8a3bc1fba21978ef
Sadraque <sdq.sts@gmail.com>
2016-11-26 18:03:58 -0200

    Finders keepers

---
+++ Intermediate/finders-keepers.js
@@ -0,0 +1,9 @@
+/*
+Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument).
+*/
+
+function findElement (arr, func) {
+  return arr.filter((e) => func(e))[0]
+}
+
+findElement([1, 2, 3, 4], function (num) { return num % 2 === 0 })
